0c4b1d8d473a28e48a31d171aa479d184599d52b
John <jooohn.eth@gmail.com>
2022-08-02 11:58:05 -0400

    Added mint token on deployment

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
 contract YourToken is ERC20 {
-    constructor() ERC20("Gold", "GLD") {
-        //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+    constructor() ERC20("King Analytics", "KING") {
+        _mint(0xcD05AE8EB5Dfed6aCaFCa65d19d99775A8a89Ed0, 1000 * 10 * 18);
     }
 }
